1e2eb48c92fa2955b680d2bb63f37bae9a6fbbaa
Jisu Kim <jisukim@uark.edu>
2023-10-17 15:43:17 -0500

    adding game start countdown timer

--- src/main/java/GameStartCountdown.java
+++ src/main/java/GameStartCountdown.java
@@ -7,27 +7,18 @@ public class GameStartCountdown {
     private static JLabel countdownLabel;
 
 //    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(GameStartCountdown::createAndShowGUI);
+//        SwingUtilities.invokeLater(GameStartCountdown::createStartCountdown);
 //    }
-    public static void createStartCounddown() {
+
+    public static void createStartCountdown() {
         JFrame frame = new JFrame("Game Start Countdown");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 700);
-        frame.setLayout(new GridBagLayout());
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(10, 0, 10, 0);
-
-        JLabel textLabel = new JLabel("Game will start in", SwingConstants.CENTER);
-        textLabel.setFont(new Font("Arial", Font.BOLD, 40));
-        frame.add(textLabel, gbc);
+        frame.setLayout(new BorderLayout());
 
         countdownLabel = new JLabel("", SwingConstants.CENTER);
         countdownLabel.setFont(new Font("Arial", Font.BOLD, 70));
-        gbc.gridy = 1;
-        frame.add(countdownLabel, gbc);
+        frame.add(countdownLabel, BorderLayout.CENTER);
 
         startCountdown();
 
@@ -37,7 +28,7 @@ public class GameStartCountdown {
 
     public static void startCountdown() {
         Timer timer = new Timer(1000, new ActionListener() {
-            int counter = 5; // starting from 5 seconds
+            int counter = 30; // starting from 30 seconds
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -47,7 +38,6 @@ public class GameStartCountdown {
                 } else {
                     ((Timer) e.getSource()).stop();
                     countdownLabel.setText("Start!");
-                    // Add your game start logic here
                 }
             }
         });
