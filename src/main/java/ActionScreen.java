4d2cf9038bf2e57c8a643db31e05bfc2b166e153
Jisu Kim <jisukim@uark.edu>
2023-11-14 11:36:42 -0600

    Merging GameStartCountdown.java into ActionScreen.java

--- src/main/java/ActionScreen.java
+++ src/main/java/ActionScreen.java
@@ -31,15 +31,14 @@ public class ActionScreen extends JFrame{
     private int remainingSeconds = 6 * 60;
     private Timer timer;
 
+    //game start countdown timer
+    private static JLabel startCountdownLabel;
+    private static JLabel textLabel;
+
+    //constructor
     ActionScreen(){
-        this.setTitle("Action Screen");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000,700);
-        this.setVisible(true);
-        this.setLocationRelativeTo(null);
-
+        createStartCountdown();
         //Vector<Player> players = new Vector<Player>();
-
         team1TableModel = new DefaultTableModel();
         team1TableModel.addColumn("Username");
         team1TableModel.addColumn("Score");
@@ -50,7 +49,75 @@ public class ActionScreen extends JFrame{
         team2TableModel.addColumn("Username");
         team2TableModel.addColumn("Score");
         team2Table = new JTable(team2TableModel);
+    }
 
+    public void createStartCountdown() {
+        JFrame frame = new JFrame("Game Start Countdown");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 700);
+        frame.setLayout(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 0, 10, 0);
+
+        textLabel = new JLabel("Game will start in", SwingConstants.CENTER);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        frame.add(textLabel, gbc);
+
+        startCountdownLabel = new JLabel("", SwingConstants.CENTER);
+        startCountdownLabel.setFont(new Font("Arial", Font.BOLD, 70));
+        gbc.gridy = 1;
+        frame.add(startCountdownLabel, gbc);
+
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+        startCountdown(frame);
+    }
+
+    public void startCountdown(JFrame frame) {
+        Timer timer = new Timer(1000, new ActionListener() {
+            int counter = 30; // starting from 30 seconds
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (counter > 1) {
+                    startCountdownLabel.setText(String.valueOf(counter));
+                    counter--;
+                } else if (counter == 1) {
+                    startCountdownLabel.setText("1");
+                    counter--;
+                } else {
+                    ((Timer) e.getSource()).stop();
+                    textLabel.setVisible(false);
+                    startCountdownLabel.setText("Start!");
+                    Timer delayTimer = new Timer(700, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            frame.dispose();
+                            //when the timer finishes, transmit code 202
+                            UdpClient udpClient = new UdpClient();
+                            udpClient.sendDataToServer(202);
+                            createActionScreen();
+                        }
+                    });
+                    delayTimer.setRepeats(false);
+                    delayTimer.start();
+                }
+            }
+        });
+        timer.setInitialDelay(0);
+        timer.start();
+    }
+
+    void createActionScreen() {
+        this.setTitle("Action Screen");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,700);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
         JPanel topPanel = new JPanel(new BorderLayout());
 
         JPanel teamScores = new JPanel();
@@ -80,6 +147,7 @@ public class ActionScreen extends JFrame{
         topPanel.add(countdownPanel, BorderLayout.EAST);
         this.add(topPanel, BorderLayout.NORTH);
         startTimer();
+
     }
 
     void addPlayers(Vector<Player> players){
